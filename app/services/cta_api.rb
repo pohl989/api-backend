ef192df00093743e4e8c6bc0ce21cdd0d073ced9
pohl989 <pohl989@gmail.com>
2020-02-16 21:50:19 -0600

    playing around some more with the train api

--- app/services/cta_api.rb
+++ app/services/cta_api.rb
@@ -15,24 +15,56 @@ class CtaApi
 
   def find_station(color: "brn", station_name: "Irving Park") 
     stations.select{|c| c[color] && c["station_name"] == station_name }
+  end
+  
+  def train_lines
+    ["red", "blue", "g", "brn", "p", "pexp", "y", "pnk", "o"]
+  end 
+
+  def stations_by_train_lines 
+    stops = stations
+    train_lines.map{|line| { line_code: line, station_names: stops.select{|stop| stop[line] }.map{|c| c["station_name"] }.uniq } }
   end 
 
   def next_train_from_home
     map_id = find_station.first["map_id"]
     train_data = trains(map_id)
     trains = train_data["ctatt"]["eta"].map do |train|
-      parse_data_from_train(train)
+      parse_train(train)
     end 
   end 
 
-  def parse_data_from_train(train)
-    arrival_time = train["arrT"] ? train["arrT"].to_time : nil
+  def next_train_from_work
+    map_id = find_station(color: "pnk", station_name: "18th").first["map_id"]
+    train_data = trains(map_id)
+    trains = train_data["ctatt"]["eta"].map do |train|
+      parse_train(train)
+    end 
+  end 
+
+  def parse_train(train)
+    arrival_time = train["arrT"] ? train["arrT"].to_datetime : nil
     human_arrival_time = arrival_time ? arrival_time.strftime("%l:%M %p") : null
+    current_time = DateTime.now
+    seconds_to_arrival = ((current_time - arrival_time) * 24 * 60 ).to_i
+    min_to_arrival = seconds_to_arrival / 60
+    msg = "#{min_to_arrival} minutes and #{seconds_to_arrival % min_to_arrival} seconds"
     destination = train["destNm"]
     is_delayed = train["isDly"] == "1" ? true : false
     is_approaching = train["isApp"] == "1" ? true : false
-    return {arrival_time: arrival_time, destination: destination, is_approaching: is_approaching, is_delayed: is_delayed, human_arrival_time: human_arrival_time}
-  end 
+    return {
+      msg: msg, 
+      arrival_time: arrival_time, 
+      destination: destination, 
+      is_approaching: is_approaching, 
+      is_delayed: is_delayed, 
+      human_arrival_time: human_arrival_time,
+      current_time: current_time,
+      seconds_to_arrival: seconds_to_arrival,
+      min_to_arrival: seconds_to_arrival / 60,
+      train_info: train,
+    }
+  end
 
 
 
